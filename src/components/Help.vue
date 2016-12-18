64b15284bbac94049479e6d6247386790121b2ce
Emmanuel Béziat <emmanuel@italic.fr>
2016-12-18 22:48:34 +0100

    fautes

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -13,44 +13,44 @@
           </ol>
         </li>
         <li><a data-scroll href="#ameliorations-et-idees">Améliorations et idées</a></li>
-        <li><a data-scroll href="#qui-es-tu">Tu es mon héro ! Mais qui es-tu ?</a></li>
+        <li><a data-scroll href="#qui-es-tu">Tu es mon héros ! Mais qui es-tu ?</a></li>
       </ol>
 
       <h2 id="a-quoi-sert-ce-site">À quoi sert ce site ?</h2>
-      <p>Avoir une image de profil personnalisée pour vos personnages, c'est classe et sympa, mais beaucoup de personnes ont utilisé cette possibilité contre les règles de NCSoft en mettant des images pornographiques. Donc, cette option a été bloquée et restreinte aux « images de personnages en jeu » seulement. Toute tentative de mettre une image externe était rejetée.</p>
-      <p>Et donc, ce petit outil vous permet d'utiliser une image de votre choix comme s'il s'agissait d'une image de votre personnage en jeu valide. ☺</p>
+      <p>Avoir une image de profil personnalisée pour vos personnages, c’est classe et sympa, mais beaucoup de personnes ont utilisé cette possibilité contre les règles de NCSoft en mettant des images pornographiques. Donc, cette option a été bloquée et restreinte aux « images de personnages en jeu » seulement. Toute tentative de mettre une image externe était rejetée.</p>
+      <p>Et donc, ce petit outil vous permet d’utiliser une image de votre choix comme s’il s’agissait d’une image de votre personnage en jeu valide. ☺</p>
 
       <div class="note note--important">Bien évidemment, n’utilisez pas mon site pour mettre des images pornographiques sur le profil de votre personnage ! Vous ne souhaiteriez pas que la possibilité de mettre une image soit de nouveau complètement bloquée, mmh ?<br> Donc soyez sages, et retenez vous de mettre des loches ou des fesses sur vos images de profil. Ca ne vous rendra <strong>clairement pas</strong> plus cool ou plus viril d’arborer une donzelle à poil. Au contraire, vous aurez juste l’air d‘un crétin glauque, stupidement incapable de contenir ses pulsions animales.</div>
 
       <h2 id="comment-proceder">Comment procéder</h2>
-      <p>Voilà comme cela se passe. Pour utiliser une image personnalisée, il nous faut les informations du jeu d'une image réelle de votre personnage, et de transférer ces informations sur votre image personnalisée. C’est exactement ce que cet outil va faire.</p>
+      <p>Voilà comme cela se passe. Pour utiliser une image personnalisée, il nous faut les informations du jeu d’une image réelle de votre personnage, et de transférer ces informations sur votre image personnalisée. C’est exactement ce que cet outil va faire.</p>
 
       <h3 id="prendre-une-photo-de-votre-personnage">Prendre une photo de votre personnage</h3>
       <p>La première chose est de prendre une photo valide et fonctionnelle depuis le jeu.</p>
-      <p>Connectez vous sur votre personnage, ouvrez votre Galerie Photo (<kbd>Echap</kbd> → <kbd>Galerie Photo</kbd>), puis cliquez sur l'icône d'appareil photo.</p>
+      <p>Connectez vous sur votre personnage, ouvrez votre Galerie Photo (<kbd>Echap</kbd> → <kbd>Galerie Photo</kbd>), puis cliquez sur l’icône d’appareil photo.</p>
       <p class="text-align-center"><img src="../assets/images/01.jpg" alt=""></p>
       <p>Une petite fenêtre s’ouvrira avec votre personnage à l’intérieur. Prenez simplement la photo en cliquant sur l’appareil photo.</p>
       <p class="text-align-center"><img src="../assets/images/02.jpg" alt=""></p>
-      <p>Votre photo a été sauvegardée dans votre dossier <kbd>CharacterShots</kbd>. Pour la retrouver, retournez sur Windows, ouvrez votre dossier <kbd>Images</kbd> folder, et vous verrez un dossier dédié à Blade &amp; Soul appelé <kbd>BnS</kbd>, dans lequel vous trouverez ce que vous cherchez</p>
+      <p>Votre photo a été sauvegardée dans votre dossier <kbd>CharacterShots</kbd>. Pour la retrouver, retournez sur Windows, ouvrez votre dossier <kbd>Images</kbd>, et vous verrez un dossier dédié à Blade &amp; Soul appelé <kbd>BnS</kbd>, dans lequel vous trouverez ce que vous cherchez</p>
       <p class="text-align-center"><img src="../assets/images/03.jpg" alt=""></p>
-      <p>Maintenant, revenez urnez sur le site <kbd>bns-profile.surge.sh</kbd>, et cliquez sur la zone en dessous de « Choisissez une image Blade &amp; Soul valide », et sélectionnez l’image valide que vous venez de prendre. Également, vous pouvez simplement prendre et faire glisser votre image dans la zone.</p>
+      <p>Maintenant, revenez sur le site <kbd>bns-profile.surge.sh</kbd>, et cliquez sur la zone en dessous de « Choisissez une image Blade &amp; Soul valide », et sélectionnez l’image valide que vous venez de prendre. Également, vous pouvez simplement prendre et faire glisser votre image dans la zone.</p>
       <p class="text-align-center"><img src="../assets/images/04.jpg" alt=""></p>
       <p>L’image apparaîtra dans la zone. Pour la changer, cliquez simplement sur l’icône de la poubelle.</p>
       <p class="text-align-center"><img src="../assets/images/05.jpg" alt=""></p>
 
       <h3 id="creer-une-image-personnalisee">Créer une image personnalisée</h3>
       <p>Maintenant, occupons-nous de votre future image de profil !</p>
-      <p>Vous aurez besoin d’un logiciel pour modifier et créer des images. Celui que j’utilise et que je vous montrerai ici est Photoshop, mais vous pouvez utiliser d’autres logiciels, à l’instar de Gimp, Photofiltre, et bien d'autres.</p>
+      <p>Vous aurez besoin d’un logiciel pour modifier et créer des images. Celui que j’utilise et que je vous montrerai ici est Photoshop, mais vous pouvez utiliser d’autres logiciels, à l’instar de Gimp, Photofiltre, et bien d’autres.</p>
       <div class="note note--info">Je ne fournirai aucune aide supplémentaire sur la façon de créer une image, ou dans l’utilisation d’un de ces logiciels. Si vous ne savez pas vous en servir, il y a de nombreux tutoriels sur internet, ou demandez sur un forum de graphisme.</div>
       <p>Tout d’abord, vous devez vous assurer que l’image que vous créez mesure <strong>378px de large</strong> and <strong>620px de haut</strong>.</p>
       <p class="text-align-center"><img src="../assets/images/06.jpg" alt=""></p>
       <p>Maintenant, vous pouvez utiliser n’importe quelle image de votre choix ; la mienne sera ce joli lapinou.</p>
       <p class="text-align-center"><img src="../assets/images/07.jpg" alt=""></p>
       <p>Ensuite, vous devez sauvegarder votre image quelque part sur votre ordinateur, par exemple sur le Bureau (c’est le plus simple pour la retrouver)</p>
-      <div class="note note--important">Assurez-vous d’enregistrer votre image au format <strong>JPEG</strong> (dont l'extension de fichier est <kbd>.jpg</kbd>) ! Si vous ne le faites pas, l’image ne sera pas convertie.</div>
+      <div class="note note--important">Assurez-vous d’enregistrer votre image au format <strong>JPEG</strong> (dont l’extension de fichier est <kbd>.jpg</kbd>) ! Si vous ne le faites pas, l’image ne sera pas convertie.</div>
       <p>Maintenant, revenez sur <kbd>bns-profile.surge.sh</kbd>, et cliquez sur la zone sous « Choisissez votre image perso ». Choisissez l’image que vous venez de créer. Encore une fois, vous pouvez également juste faire glisser l’image sur ladite zone.</p>
       <p class="text-align-center"><img src="../assets/images/08.jpg" alt=""></p>
-      <p>Votre image apparaîtra dans la zone, à coté de la première. Enfin, cliquez sur le bouton d'envoi pour lancer la conversion.</p>
+      <p>Votre image apparaîtra dans la zone, à coté de la première. Enfin, cliquez sur le bouton d’envoi pour lancer la conversion.</p>
       <p class="text-align-center"><img src="../assets/images/09.jpg" alt=""></p>
 
       <h3 id="utiliser-son-image-personnalisee-en-jeu">Utiliser son image personnalisée en jeu</h3>
@@ -67,22 +67,22 @@
       <p>Appuyez syr <kbd>F2</kbd> pour voir votre profil public. Votre image n’est pas forcément à jour, donc cliquez sur le bouton actualiser.</p>
       <p class="text-align-center"><img src="../assets/images/13.jpg" alt=""></p>
       <p>Et c’est tout !</p>
-      <h4>Maintenant que vous êtes prêt(e), vous pouvez <a v-link="{ name: 'home' }">retourner à l’accueile</a></h4>
+      <h4>Maintenant que vous êtes prêt(e), vous pouvez <a v-link="{ name: 'home' }">retourner à l’accueil.</a></h4>
 
       <h2 id="ameliorations-et-idees">Améliorations et idées</h2>
       <p>Je travaillerai sur cet outil au fur et à mesure. Actuellement, j’ai quelques points à travailler dont :</p>
       <ul>
         <li>Convertir le format de l’image si l’utilisateur en donne un mauvais</li>
       </ul>
-      <p>Vous pouvez voir mon travail sur le <a href="https://github.com/EmmanuelBeziat/bns-profile/issues">projet github</a>. Si vous avez une idée ou seggestion, ou si vous avez trouvé un problème, vous pouvez <a href="https://github.com/EmmanuelBeziat/bns-profile/issues/new">créer un nouveau ticket</a> pour qu’on puisse en discuter.</p>
+      <p>Vous pouvez voir mon travail sur le <a href="https://github.com/EmmanuelBeziat/bns-profile/issues">projet github</a>. Si vous avez une idée ou suggestion, ou si vous avez rencontré un problème, vous pouvez <a href="https://github.com/EmmanuelBeziat/bns-profile/issues/new">créer un nouveau ticket</a> pour qu’on puisse en discuter.</p>
 
-      <h2 id="qui-es-tu">Tu es mon héro ! Mais qui es-tu ?</h2>
+      <h2 id="qui-es-tu">Tu es mon héros ! Mais qui es-tu ?</h2>
       <p>Je suis un développeur français de 29 ans, vivant sous le chaud soleil du sud, avec les pieds dans la mer Méditerrannée.</p>
-      <p>Je joue à Blade &amp; Sould depuis plus de deux ans, et j’adore avoir une belle image de profil personnalisée pour mes personnages. C’est pourquoi j'ai créé ce petit outil, basé sur le travail de <a href="https://github.com/f4rnham/">f4rnham</a>.</p>
+      <p>Je joue à Blade &amp; Soul depuis plus de deux ans, et j’adore avoir une belle image de profil personnalisée pour mes personnages. C’est pourquoi j'ai créé ce petit outil, basé sur le travail de <a href="https://github.com/f4rnham/">f4rnham</a>.</p>
       <p>Si vous croisez un Lyn Lame Dansante dans le jeu, faites-moi un coucou ! ☺</p>
 
       <h3>Mais aussi…</h3>
-      <p>Un gros merci à <strong>Rellenna</strong> pour le temps qu’il a pris afin de proposer une belle traduction en français !</p>
+      <p>Un gros merci à <strong>Rellenna</strong> pour le temps qu’il a prit afin de proposer une belle traduction en français !</p>
     </div>
 
     <div class="help-en" v-else>
